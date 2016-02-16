748872e11944d1092417ade062830550cccf11a0
Colton Allen <cmanallen90@gmail.com>
2016-02-16 13:47:50 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-"""
-Flask-SQLAlchemy-JSONAPI
-------------------------
+"""Flask-SQLAlchemy-JSONAPI.
 
 A URL parsing library that uses documented JSONAPI 1.0 specification
 URL parameters to filter, sort, and include JSONAPI response objects.
 """
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 setup(
     name='Flask-SQLAlchemy-JSONAPI',
     version='0.1',
-    url='',
+    url='https://github.com/caxiam/sqlalchemy-jsonapi-collections',
     license='BSD',
     author='Colton Allen',
     author_email='colton.allen@caxiam.com',
     description='A collection response filtering library.',
     long_description=__doc__,
-    py_modules=['flask_sqlalchemy_jsonapi'],
+    packages=find_packages(exclude=("test*", )),
+    package_dir={'flask-sqlalchemy-jsonapi': 'flask-sqlalchemy-jsonapi'},
     zip_safe=False,
     include_package_data=True,
     platforms='any',
@@ -32,5 +31,6 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+    ],
+    test_suite='tests'
 )
