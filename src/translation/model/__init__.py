bf88003114a0008d01f0cc570a77cff9012ab65b
Colton Allen <cmanallen90@gmail.com>
2016-06-26 12:12:41 -0400

    Decoupled model driver from filter and sort data types

--- src/translation/model/__init__.py
+++ src/translation/model/__init__.py
@@ -3,25 +3,14 @@
 class BaseModelDriver(object):
     """Base model driver."""
 
-    def __init__(self, model):
+    def __init__(self, model, default_attribute='id'):
         """Setup the driver.
 
         :param model: SQLAlchemy model reference.
         """
         self.model = model
+        self.default_attribute = default_attribute
 
-    def parse_filters(self, filters):
-        """Parse a set of filter triples."""
-        raise NotImplementedError
-
-    def parse_filter(self, path, strategy, values):
-        """Parse a filter triple's path to a column reference."""
-        raise NotImplementedError
-
-    def parse_sorts(self, sorts):
-        """Parse a set of sort doubles."""
-        raise NotImplementedError
-
-    def parse_sort(self, path, direction):
-        """Parse a sort double's path to a column reference."""
+    def parse_path(self, path):
+        """Parse a string path to a column attribute."""
         raise NotImplementedError
--- src/translation/model/sqlalchemy.py
+++ src/translation/model/sqlalchemy.py
@@ -5,48 +5,36 @@ from src.translation.model import BaseModelDriver
 class SQLAlchemyModelDriver(BaseModelDriver):
     """Translate string paths to column references."""
 
-    default_attribute = 'id'
+    def parse_path(self, path):
+        """Parse a string path to a column attribute.
 
-    def parse_filters(self, filters):
-        """Parse a set of filter triples."""
-        parsed_filters = []
-        for path, strategy, values in filters:
-            parsed_filters.append(self.parse_filter(path, strategy, values))
-        return parsed_filters
+        Return a triple of a column attribute, the intermediary models,
+        and the list of joins required to get the attribute.
 
-    def parse_filter(self, path, strategy, values):
-        """Parse a filter triple's path to a column reference."""
-        reference = self.model
-        for stone in path.split('.'):
-            reference = getattr(reference, stone)
-        return reference
-
-    def parse_sorts(self, sorts):
-        """Parse a set of sort doubles."""
-        parsed_sorts = []
-        for path, direction in sorts:
-            parsed_sorts.append(self.parse_sort(path, direction))
-        return parsed_sorts
-
-    def parse_sort(self, path, direction):
-        """Parse a sort double's path to a column reference."""
+        :param path: A dot seperated string representing the attributes
+                     of a model class.
+        """
         stones = path.split('.')
         relationships, attribute = stones[:-1], stones[-1]
 
-        joins = []
         model = self.model
+        joins = []
+        models = []
         for relationship in relationships:
             reference = getattr(model, relationship)
             if not self._is_relationship(reference):
                 raise TypeError('Invalid relationship specified.')
             model = self._get_relationship_class(reference)
+            models.append(model)
             joins.append(model.__tablename__)
+
         column = getattr(model, attribute)
         if self._is_relationship(column):
             model = self._get_relationship_class(column)
-            column = getattr(model, self.default_attribute)
+            models.append(model)
             joins.append(model.__tablename__)
-        return (column, direction, joins)
+            column = getattr(model, self.default_attribute)
+        return (column, models, joins)
 
     def _is_relationship(self, relationship):
         """Return `True` if a relationship mapper was specified."""
