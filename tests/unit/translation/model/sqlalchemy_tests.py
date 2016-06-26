05ff45a5f7ccfb405f80051bf0dd7004325b72e0
Colton Allen <cmanallen90@gmail.com>
2016-06-26 12:13:32 -0400

    Added error coverage

--- tests/unit/translation/model/sqlalchemy_tests.py
+++ tests/unit/translation/model/sqlalchemy_tests.py
@@ -1,4 +1,4 @@
-"""."""
+"""SQLAlchemy model driver module."""
 from src.translation.model.sqlalchemy import SQLAlchemyModelDriver
 from tests.sqlalchemy import BaseSQLAlchemyTestCase, Person, School, Student
 
@@ -15,32 +15,48 @@ class SQLAlchemyModelTestCase(BaseSQLAlchemyTestCase):
 
     def test_attribute_column(self):
         """Test getting an attribute column."""
-        sort = ('name', '+')
-        column, _, joins = self.driver.parse_sort(*sort)
+        column, models, joins = self.driver.parse_path('name')
 
         self.assertTrue(column == Person.name)
+        self.assertTrue(models == [])
         self.assertTrue(joins == [])
 
     def test_nested_attribute_column(self):
         """Test getting a related attribute column."""
-        sort = ('student.school_id', '+')
-        column, _, joins = self.driver.parse_sort(*sort)
+        column, models, joins = self.driver.parse_path('student.school_id')
 
         self.assertTrue(column == Student.school_id)
+        self.assertTrue(models == [Student])
         self.assertTrue(joins == ['student'])
 
     def test_deeply_nested_attribute_column(self):
         """Test getting a deeply related attribute column."""
-        sort = ('student.school.name', '+')
-        column, _, joins = self.driver.parse_sort(*sort)
+        column, models, joins = self.driver.parse_path('student.school.name')
 
         self.assertTrue(column == School.name)
+        self.assertTrue(models == [Student, School])
         self.assertTrue(joins == ['student', 'school'])
 
     def test_missing_attribute_column(self):
         """Test getting a missing attribute's default column."""
-        sort = ('student', '+')
-        column, _, joins = self.driver.parse_sort(*sort)
+        column, models, joins = self.driver.parse_path('student')
 
         self.assertTrue(column == Student.id)
+        self.assertTrue(models == [Student])
         self.assertTrue(joins == ['student'])
+
+    def test_unknown_attribute(self):
+        """Test getting an attribute that doesn't exist on the object."""
+        try:
+            self.driver.parse_path('height')
+            self.assertTrue(False)
+        except AttributeError:
+            self.assertTrue(True)
+
+    def test_column_as_relationship(self):
+        """Test getting a relationshiup that doesn't exist on the model."""
+        try:
+            self.driver.parse_path('age.id')
+            self.assertTrue(False)
+        except TypeError:
+            self.assertTrue(True)
