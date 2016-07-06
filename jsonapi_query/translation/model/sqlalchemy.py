9749f15549e52eb8e9660fe072f2ce7a07664c5b
Colton Allen <cmanallen90@gmail.com>
2016-07-05 22:35:23 -0400

    Added support for returning join conditions

--- jsonapi_query/translation/model/sqlalchemy.py
+++ jsonapi_query/translation/model/sqlalchemy.py
@@ -22,18 +22,21 @@ class SQLAlchemyModelDriver(BaseModelDriver):
         relationships, attribute = stones[:-1], stones[-1]
 
         model = self.model
+        joins = []
         models = []
         for relationship in relationships:
             reference = self._get_relationship(relationship, model)
+            joins.append(self._extract_join(reference, model))
             model = self._get_relationship_class(reference)
             models.append(model)
 
         column = self._get_column(attribute, model)
         if self._is_relationship(column):
+            joins.append(self._extract_join(column, model))
             model = self._get_relationship_class(column)
             models.append(model)
             column = self._get_column(self.default_attribute, model)
-        return (column, models)
+        return (column, models, joins)
 
     def _get_column(self, attribute, model):
         try:
@@ -58,3 +61,15 @@ class SQLAlchemyModelDriver(BaseModelDriver):
         except AttributeError:
             return False
         return True
+
+    def _extract_join(self, mapper, model):
+        """Extract the join condition for a given relationship mapper."""
+        # If the mapper is not self-referential we can return it.
+        if model != self._get_relationship_class(mapper):
+            return mapper
+
+        # If the mapper is self-referential we want to join its backref.
+        if mapper.property.backref is not None:
+            return mapper.property.backref
+        else:
+            return mapper.property.back_populates
