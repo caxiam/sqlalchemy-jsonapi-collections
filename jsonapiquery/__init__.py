8669265d4b983e1d6f12127062eede3d30ba36ab
Colton Allen <cmanallen90@gmail.com>
2018-10-22 10:13:51 -0700

    Add pagination link construction

--- jsonapiquery/__init__.py
+++ jsonapiquery/__init__.py
@@ -1,4 +1,5 @@
 from jsonapiquery import url
+from urllib.parse import urlencode
 
 
 def iter_by_type(iterator, params, drivers):
@@ -10,21 +11,25 @@ def iter_by_type(iterator, params, drivers):
 
 def filter_query(query, params, drivers):
     filters = iter_by_type(url.iter_filters, params, drivers)
+    filters = list(filters)
     return query.apply_filters(filters), filters
 
 
 def sort_query(query, params, drivers):
     sorts = iter_by_type(url.iter_sorts, params, drivers)
+    sorts = list(sorts)
     return query.apply_sorts(sorts), sorts
 
 
 def include_query(query, params, drivers):
     includes = iter_by_type(url.iter_includes, params, drivers)
+    includes = list(includes)
     return query.apply_includes(includes), includes
 
 
 def paginate_query(query, params):
     paginators = url.iter_paginators(params)
+    paginators = list(paginators)
     return query.apply_paginators(paginators), paginators
 
 
@@ -54,3 +59,69 @@ def serialize_relationship(mapper, relationship, model):
     if not isinstance(models, list):
         models = [models]
     return relationship.serialize(models), models
+
+
+def make_pagination_links(base_url, paginators, parameters, total):
+    if 'page[number]' in parameters:
+        links = _paginate_number(base_url, paginators, parameters, total)
+    else:
+        links = _paginate_offset(base_url, paginators, parameters, total)
+
+    links['self'] = base_url
+    return links
+
+
+def _paginate_offset(base_url, paginators, parameters, total):
+    limit, offset = 50, 0
+    for paginator in paginators:
+        if paginator.strategy == 'limit':
+            limit = int(paginator.value)
+        elif paginator.strategy == 'offset':
+            offset = int(paginator.value)
+
+    url = base_url + '?{}'
+    values = _build_page_offset_values(total, limit, offset)
+    return _build_urls(_encode_page_offset, url, parameters, values)
+
+
+def _paginate_number(base_url, paginators, parameters, total):
+    limit, page = 50, 1
+    for paginator in paginators:
+        if paginator.strategy == 'limit':
+            limit = int(paginator.value)
+        elif paginator.strategy == 'number':
+            page = int(paginator.value)
+
+    url = base_url + '?{}'
+    values = _build_page_number_values(total, limit, page)
+    return _build_urls(_encode_page_number, url, parameters, values)
+
+
+def _build_urls(fn, base_url, parameters, values):
+    return {
+        'first': base_url.format(fn(parameters, values[0])),
+        'last': base_url.format(fn(parameters, values[1])),
+        'next': base_url.format(fn(parameters, values[2])),
+        'prev': base_url.format(fn(parameters, values[3]))
+    }
+
+
+def _build_page_offset_values(total, limit, offset):
+    return 0, max(total - limit, 0), offset + limit, max(offset - limit, 0)
+
+
+def _build_page_number_values(total, limit, current):
+    return 1, max(total / limit, 1), current + 1, max(current - 1, 1)
+
+
+def _encode_page_offset(params, value):
+    return _encode_parameters(params, 'page[offset]', value)
+
+
+def _encode_page_number(params, value):
+    return _encode_parameters(params, 'page[number]', value)
+
+
+def _encode_parameters(params, key, value):
+    params[key] = value
+    return urlencode(params)
